4cf58d1f73301ac43eb6309aaf479a967295a1b5
Christina Kang <cykang90@gmail.com>
2020-02-03 17:01:21 -0500

    created homepage component

---
+++ client/components/homepage.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+class Home extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      location: '',
+      startDate: '',
+      endDate: '',
+      interests: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit() {
+    //THUNK CREATOR, GET PLACES
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="location">Location:</label>
+          <input
+            type="text"
+            name="location"
+            value={this.state.location}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="start">Start Date:</label>
+          <input
+            type="date"
+            name="startDate"
+            value={this.state.startDate}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="end">End Date:</label>
+          <input
+            type="date"
+            name="endDate"
+            value={this.state.endDate}
+            onChange={this.handleChange}
+          />
+
+          <label htmlFor="arts">Arts</label>
+          <input type="checkbox" name="arts" value="arts" />
+          <label htmlFor="shopping">Shopping</label>
+          <input type="checkbox" name="shopping" value="shopping" />
+          <label htmlFor="nature">Nature</label>
+          <input type="checkbox" name="nature" value="nature" />
+          <label htmlFor="culture">Culture</label>
+          <input type="checkbox" name="culture" value="culture" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Home
