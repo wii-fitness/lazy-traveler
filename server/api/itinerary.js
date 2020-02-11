7b48dd7b79209cb5d0a8a7762475c31b3d1fa281
Henry Chong <chong.henry@gmail.com>
2020-02-10 23:43:16 -0500

    userSaveFunction v1

--- server/api/itinerary.js
+++ server/api/itinerary.js
@@ -13,16 +13,16 @@ router.get('/', async (req, res, next) => {
 })
 
 //return & await
-router.post('/', async (req, res, next) => {
-  try {
-    const places = req.body.places
-    for (place of places) {
-      Place.findOrCreate(place.id)
-    }
-  } catch (err) {
-    next(err)
-  }
-})
+// router.post('/', async (req, res, next) => {
+//   try {
+//     const places = req.body.places
+//     for (place of places) {
+//       Place.findOrCreate(place.id)
+//     }
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
 router.get('/:userId', async (req, res, next) => {
   try {
@@ -32,6 +32,41 @@ router.get('/:userId', async (req, res, next) => {
       }
     })
     res.json(itinerary)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// route that saves a user's itinerary
+// post (city,location and userId) in itinerary table
+// post in itineraryplaces table (itineraryId = itineraryId, placeId = google.id)
+// do we need to post in the places table?
+router.post('/:userId', async (req, res, next) => {
+  try {
+    const data = req.body.places
+    let googlePlaceId = []
+
+    // need to fix db to use googleId (string vs integer)
+    for (let i = 0; i < data.length; i++) {
+      googlePlaceId.push(data[i].id)
+    }
+
+    // creates new instance in the Itinerary table
+    await Itinerary.create({
+      city: 'need to update',
+      arrival: '2020-02-20',
+      departure: '2020-02-20',
+      timeOfStay: 2,
+      userId: req.params.userId
+    }).then(insertedItinerary => {
+      ItineraryPlace.bulkCreate([
+        {
+          itineraryId: insertedItinerary.dataValues.id,
+          placeId: 1
+        }
+      ])
+    })
+    res.status(201)
   } catch (err) {
     next(err)
   }
