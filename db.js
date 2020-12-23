296859f0574aa08a7d1ece65ec3cd671ccf7c4f6
Bilal Amjad <bilal.amjad@meritincentives.com>
2020-12-23 13:14:08 +0500

    github code

--- db.js
+++ db.js
@@ -4,9 +4,10 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
+  //process.env.DATABASE_URL ||
+  //`postgres://hrzzcyopnspdxs:ea97b365a27eea850063ee5ef76e4f6c15de1f367ac4c5e25853cd0297c2bbdb@ec2-54-172-219-218.compute-1.amazonaws.com:5432/d2rdujiodfnmqb`,
   process.env.DATABASE_URL ||
-    `postgres://hrzzcyopnspdxs:ea97b365a27eea850063ee5ef76e4f6c15de1f367ac4c5e25853cd0297c2bbdb@ec2-54-172-219-218.compute-1.amazonaws.com:5432/d2rdujiodfnmqb`,
-  //process.env.DATABASE_URL || `postgres://postgres:postgres@localhost:5432/${databaseName}`,
+    `postgres://postgres:postgres@localhost:5432/${databaseName}`,
   {
     logging: false
   }
